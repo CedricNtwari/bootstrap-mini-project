19ff1b9ccbb36e1162bb946c9c74792a16986b29
CedricNtwari <cedric.ntwari@powercoders.org>
2024-04-29 21:14:12 +0200

    Add properties to send email

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,17 @@
-console.log('sendEmail.js loaded')
+function sendMail(contactForm) {
+  emailjs
+    .send('service_3qsjmej', 'template_t5lrlhb', {
+      from_name: contactForm.name.value,
+      reply_to: contactForm.emailaddress.value,
+      message: contactForm.projectsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log('SUCCESS', response)
+      },
+      function (error) {
+        console.log('FAILED', error)
+      },
+    )
+  return false // To block from loading a new page
+}
